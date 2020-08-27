e57a735606f08a057a23173178d7c916a8f991f8
Lucas Nunes Camargo <Lucas@MacBook-Pro-de-Lucas.local>
2020-08-26 23:59:04 -0300

    Endpoints funcionais

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,55 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) {
+      throw new AppError('This customer does not exists.');
+    }
+
+    const findedProducts = await this.productsRepository.findAllById(products);
+
+    if (products.length !== findedProducts.length) {
+      throw new AppError('One or more products in request does not exists.');
+    }
+
+    const productsQuantityUpdated: IProduct[] = [];
+    const normalizedProducts = findedProducts.map((product, i) => {
+      const requestProduct = products[i];
+      const quantity = product.quantity - requestProduct.quantity;
+
+      if (quantity < 0) {
+        throw new AppError(
+          `Product ${product.name} not have sulficient quantity.`,
+        );
+      }
+      productsQuantityUpdated.push({
+        id: product.id,
+        quantity,
+      });
+      return {
+        product_id: product.id,
+        ...product,
+        quantity: requestProduct.quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: normalizedProducts,
+    });
+
+    await this.productsRepository.updateQuantity(productsQuantityUpdated);
+
+    return order;
   }
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,22 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const hasProduct = await this.productsRepository.findByName(name);
+    if (hasProduct) {
+      throw new AppError('Another product with same name already exists.');
+    }
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+    return product;
   }
 }
 
